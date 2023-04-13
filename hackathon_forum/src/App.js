d66216308840398df25c9700efa2edaeb8058bad
Sarthakls9 <109941152+Sarthakls9@users.noreply.github.com>
2023-04-13 10:36:28 +0530

    commit

---
+++ hackathon_forum/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SideNav from './sidenav.js'
+
+function App() {
+  return (
+    <div className="App">
+      <SideNav/>
+    </div>
+  );
+}
+
+export default App;
---
+++ hackathon_forum/src/sidenav.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function SideNav() {
+  return (
+    <div className='SideNav'>
+        <div className='sideNav_logo'>
+            <img src=''></img>
+        </div>
+        <div className='sideNav_img'>
+            <img src=''></img>
+        </div>
+        <div className='sideNav_details'>
+            Name:
+            Age:
+        </div>
+        
+
+    </div>
+  )
+}
